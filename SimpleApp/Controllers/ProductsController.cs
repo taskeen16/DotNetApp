adb279e4af36fbba6e6de48a566434c6a4052e5b
taskeen16 <65477432+taskeen16@users.noreply.github.com>
2020-07-17 12:24:22 +0530

    Add files via upload

---
+++ SimpleApp/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SimpleApp.Models;
+
+namespace SimpleApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        List<Product> _products = new List<Product>()
+        {
+            new Product(){ProductID = 0, ProductName= "Laptop", ProductPrice="500" },
+            new Product(){ProductID = 1, ProductName= "TV", ProductPrice="200" }
+    };
+        public IEnumerable<Product> Get()
+        {
+            return _products;
+        }
+    }
+}
